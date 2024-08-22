068a44718b3d063b227ad2dbc1fbdfff6f53c58c
Levi <levibezerra320@gmail.com>
2024-08-22 13:33:44 -0300

    criação do componente de regionais e começo das rotas

---
+++ src/components/regionais.jsx
@@ -0,0 +1,366 @@
+const regionals = [
+    {
+        titulo: "Regional 01 - Fortaleza",
+        link: "/admontese.com/admontesegestao/r01",
+        imagem: "https://lh6.googleusercontent.com/37Rl7nOuYeB5U6JhlqB1uU0FeKcajeRs5Z2OhN12DHEY0Ixn7AjM-tplQTgJ2wsSzGsVNGE5P39qF4hNH_ZJl0NSHPOuK7Skrds6_1uFnYlXKYjCumiG29qAsII1xIWBrA=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r01/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r01/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r01/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 02 - Jardim",
+        link: "/admontese.com/admontesegestao/r02",
+        imagem: "https://lh6.googleusercontent.com/37Rl7nOuYeB5U6JhlqB1uU0FeKcajeRs5Z2OhN12DHEY0Ixn7AjM-tplQTgJ2wsSzGsVNGE5P39qF4hNH_ZJl0NSHPOuK7Skrds6_1uFnYlXKYjCumiG29qAsII1xIWBrA=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r02/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r02/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r02/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 03 - Tianguá",
+        link: "/admontese.com/admontesegestao/r03",
+        imagem: "https://lh6.googleusercontent.com/37Rl7nOuYeB5U6JhlqB1uU0FeKcajeRs5Z2OhN12DHEY0Ixn7AjM-tplQTgJ2wsSzGsVNGE5P39qF4hNH_ZJl0NSHPOuK7Skrds6_1uFnYlXKYjCumiG29qAsII1xIWBrA=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r03/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r03/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r03/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 04 - Camocim",
+        link: "/admontese.com/admontesegestao/r04",
+        imagem: "https://lh6.googleusercontent.com/37Rl7nOuYeB5U6JhlqB1uU0FeKcajeRs5Z2OhN12DHEY0Ixn7AjM-tplQTgJ2wsSzGsVNGE5P39qF4hNH_ZJl0NSHPOuK7Skrds6_1uFnYlXKYjCumiG29qAsII1xIWBrA=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r04/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r04/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r04/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 05 - Sobral",
+        link: "/admontese.com/admontesegestao/r05",
+        imagem: "https://lh6.googleusercontent.com/37Rl7nOuYeB5U6JhlqB1uU0FeKcajeRs5Z2OhN12DHEY0Ixn7AjM-tplQTgJ2wsSzGsVNGE5P39qF4hNH_ZJl0NSHPOuK7Skrds6_1uFnYlXKYjCumiG29qAsII1xIWBrA=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r05/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r05/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r05/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 06 - Acaraú",
+        link: "/admontese.com/admontesegestao/r06",
+        imagem: "https://lh5.googleusercontent.com/Az20Ts7k6yUh9y-8tWuuala9TxhbRgP_SDW4rLwMtIKYJ8c7PvMYlaRESGYZSckmag732eM-JIUzIsjmZWjJaZwrX6cdKbhZE-oZ4FvlD_yS4xVPxuDIhJz_UFFgt0rzgg=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r06/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r06/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r06/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 07 - Beberibe",
+        link: "/admontese.com/admontesegestao/r07",
+        imagem: "https://lh5.googleusercontent.com/U2h_t9fb5SiTFkrxyEHnFi6qVvsVptYwBCrDuiWVloguzT0vi5xrFYFgrSg7OUxYVwRFeHz8rC_nfc1UdkW6jy4hgeTDUCtO5vuZYPJWFb1tuR_i5cXSsMWO8DjjBewm=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r07/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r07/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r07/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 08 - Iguatu",
+        link: "/admontese.com/admontesegestao/r08",
+        imagem: "https://lh3.googleusercontent.com/Rt65RdeVSFLA58LD6cxa7Iwyvi3vdiLB2c3_nO5y8cZIopnDJYABvuAnfQ_aRsO_J-RkNfJvo_VV0g-D8hrxQrYMovRmE2O9QkLJ3S1vH-gY9h6w6RSfo2PDNqrkgIWaTQ=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r08/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r08/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r08/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 09 - Quixeramobim",
+        link: "/admontese.com/admontesegestao/r09",
+        imagem: "https://lh3.googleusercontent.com/BPNkURZPUWgs7ezm-YkQ2LKpt_5n0eU1JlspN2NoEuDLy95oIsH8R2iLt_dHBbpZ9xW2gPa-7e5BPebBl9EKZPOpDTwwonirTOdZjTjjLZtrwvOpG92kmD-g_UBHGKxhtw=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r09/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r09/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r09/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 10 - Crateús",
+        link: "/admontese.com/admontesegestao/r10",
+        imagem: "https://lh3.googleusercontent.com/T6jsQpo7FSb7GdwPKypaEsY8qQZXOBjUgHYO08YatQ5P4pJtvpY6cyRtfVYMYk2QU61psV5h76Bq6Bo3vwtdymrn7Q5aUb95CkPM1yLiX1j7Y1CRNAHDklY_41HgPdEfxg=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r10/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r10/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r10/patrimonio"
+            }
+        ]
+    },
+
+    {
+        titulo: "Regional 11 - Itapipoca",
+        link: "/admontese.com/admontesegestao/r11",
+        imagem: "https://lh3.googleusercontent.com/KU2OuhhxRmT8mfNeS0bbG9ScFhJZJwvM9C1HPhGQwTrlFjJng1sxsH-MQZmYTHGPzW49xy3FgJ2F0bcmr5B92lfT90rcoIv1UCWwGRBGexRVz8DkP2BvlApkVKnAwovEmQ=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r11/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r11/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r11/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 12 - Canindé",
+        link: "/admontese.com/admontesegestao/r12",
+        imagem: "https://lh3.googleusercontent.com/D1o6XJ6XEtH1vA_jw1lnA4VKrDPVnP6DTzM-QNzKFaZKdzTE66zZmy-JIaAzUilLG1dKYczpJ4yMa7Q6JPSlMv9z6qCQ9cxNlbkg1qS_ayRUJm-7v5bdHMo3X4V_VzvIoQ=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r12/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r12/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r12/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 13 - Limoeiro do Norte",
+        link: "/admontese.com/admontesegestao/r13",
+        imagem: "https://lh3.googleusercontent.com/p6vBwU6D6T_NPp64Pm6rgReBb9g4_b38JTRKjoDQ_U5ltlW9jPnZOEY4TmULcbwVemWyqPxhbJ2VUKuY1bdOzy6lvZJpPQ_5F6GrvNf5EZ1Whp1cUdrzEGpke8DZb5oxJQ=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r13/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r13/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r13/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 14 - Maranguape",
+        link: "/admontese.com/admontesegestao/r14",
+        imagem: "https://lh3.googleusercontent.com/qXyP-KQadZl7ZYBz2USf5sETgGeebcfhD1C64k5mJSwCZ2bAk2rwvOsrylgf9ykeAHuHTwDyxqCk2S_R2UuNp3LeolXZZ9neXUdmCZv9rCvPfDtCxQTXN8yzJ4ArbZzIsQ=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r14/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r14/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r14/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 15 - Pacajus",
+        link: "/admontese.com/admontesegestao/r15",
+        imagem: "https://lh3.googleusercontent.com/W0Xc-YrAmvcjk1L6iqHvmlJ2hprgZj90yrQDKx57r3PN_cAxZwmf2l2PQo0XZcCp6_YANqsdFsjPShZ3Tyo04X6kAtXTqvKn1oGnJwqUlEL_pQDBmVjqSOqB5hvXRf-WLQ=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r15/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r15/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r15/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 16 - Russas",
+        link: "/admontese.com/admontesegestao/r16",
+        imagem: "https://lh3.googleusercontent.com/cQ6whtvLs6g2w9V9g0-19T9hVkKFTBdDfcMjZkX6YscKfHl1SB5U8pOHQarEZUtHoJ5lHpD8qKrqe_VUuHLy5cFiZytNW6K3hTuIC8KhZ8HQI8N19ne1BJ8AuyfUz2MZJw=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r16/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r16/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r16/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 17 - Morada Nova",
+        link: "/admontese.com/admontesegestao/r17",
+        imagem: "https://lh3.googleusercontent.com/J6InDXbQf7f7zO2dA1mG_sleAFLdVJcWTrmQHVlQMIhSn3FTL7Jgrvzr7JFLvWz6XGz8sZZjZVsebsPH6KFX8H8JrbnnKPKBy5CBZ-6pJZa9OFO7vMuSwXTyRk7cNH0lZg=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r17/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r17/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r17/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 18 - Horizonte",
+        link: "/admontese.com/admontesegestao/r18",
+        imagem: "https://lh3.googleusercontent.com/HhNHQvj2w90WsPLh3ccShf7EVf7oeIN9CwOvmtCkGi5GppzJtkdfHn62aYxcklVCzHNPev2oyCWJ9d4aSZ65SyXNh8cdqvH0ZMYMrrZUnS_4RI8Qw_NnCWfCN9H_RoygCw=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r18/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r18/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r18/patrimonio"
+            }
+        ]
+    },
+    {
+        titulo: "Regional 19 - Quixadá",
+        link: "/admontese.com/admontesegestao/r19",
+        imagem: "https://lh3.googleusercontent.com/FYt2nFxk9JdhAnG-kGb43XPhHd6HQvG3zdfWgL8dNNROJqRtGJcBSs_a8wvKppvX9XtGsh52DRFo5woC6N4TZUjKBZHGslNzF9SB_K7Z8_Johj04G7EmGDlfnO1jV4KMow=w1280",
+        opcoes: [
+            {
+                titulo: "Tesouraria Regional",
+                link: "/admontese.com/admontesegestao/r19/tesouraria"
+            },
+            {
+                titulo: "Controle de Membros",
+                link: "/admontese.com/admontesegestao/r19/membros"
+            },
+            {
+                titulo: "Relatório Patrimonial",
+                link: "/admontese.com/admontesegestao/r19/patrimonio"
+            }
+        ]
+    }
+];
+
+export default regionals
