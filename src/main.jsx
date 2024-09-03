ecb10b5c16b64fb4740cf048ef5653c891227354
Misael <misaelfernandes2023@gmail.com>
2024-09-03 12:21:19 -0300

    fix atalho

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import LandingPage from './LandingPage.jsx'
 import Layout from './pages/Layout.jsx'
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// Registre o Service Worker para habilitar o PWA
+serviceWorkerRegistration.register();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
